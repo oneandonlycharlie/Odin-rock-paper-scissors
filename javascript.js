be39bb813cb5dc7930ae082f59c7c5326bb122e6
oneandonlycharlie <160505188+oneandonlycharlie@users.noreply.github.com>
2024-08-14 19:07:28 +0800

    feat: create a rock-paper-scissor game
    a human vs computer game to play in the console, single round only.

---
+++ javascript.js
@@ -0,0 +1,38 @@
+console.log("Rock Paper Scissor game")
+
+function getComputerChoice() {
+    let a = Math.random()
+    if (a <= 0.3) {
+        return "ROCK"
+    }
+    else if (a >0.3 && a <= 0.6) {
+        return "PAPER"
+    }
+    else return 'SCISSORS'
+}
+
+function getHumanChoice() {
+    let userInput = prompt("What would you like to choose? Rock/Paper/Scissors")
+    return userInput.toUpperCase()
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log(`You both chose ${ humanChoice }, it's a tie`);
+    } else if (humanChoice == "PAPER" && computerChoice == "ROCK" 
+        || humanChoice == "ROCK" && computerChoice == "SCISSORS" 
+        || humanChoice == "SCISSORS" && computerChoice == "PAPER") {
+        console.log(`You win! ${ humanChoice } beats ${ computerChoice }`);
+        humanScore++;
+    } else 
+        console.log(`You lose! ${ computerChoice } beats ${ humanChoice }`);
+        computerScore++;
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
